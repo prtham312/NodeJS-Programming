21091fca4d31ef9de85927a86f758917c74d71b8
prtham312 <pb3488@srmist.edu.in>
2025-04-08 12:45:55 +0530

    fs.writefile() and fs.unlink has been implemented

--- filesystem.js
+++ filesystem.js
@@ -22,12 +22,16 @@ var fs = require('fs');
 //     console.log('Saved!');
 //   });
 
+//fs.append data to a file and if file is not present it will create a new file
+
+
 //-------------------------------------------------------------------------------------------------------------------------------//
 
-fs.open('mynewfile2.txt' , 'w' , function(err, file){
-    if (err) throw err;
-  console.log('Saved!');
-})
+// fs.open('mynewfile2.txt' , 'w' , function(err, file){
+//     if (err) throw err;
+//   console.log('Saved!');
+// })
+
 
 /*has modes like append or read
 'r'	Open file for reading. Error if it doesn't exist.
@@ -38,10 +42,17 @@ fs.open('mynewfile2.txt' , 'w' , function(err, file){
 'a+'	Read + append.
 */
 
+//-------------------------------------------------------------------------------------------------------------------------------//
+
+// fs.writeFile('mynewfile2.txt' , 'Hello vernova' ,  function(err){
+//     if(err) throw err;
+//     console.log("saved");
+// })
+//The fs.writeFile() method replaces the specified file and content if it exists. If the file does not exist, a new file, containing the specified content, will be created:
 
 //-------------------------------------------------------------------------------------------------------------------------------//
 
-/* 
-will throw an error if file not found
-fs.append data to a file and if file is not present it will create a new file
-*/
+fs.unlink('mynewfile2.txt' , function(err){
+    if(err) throw err;
+    console.log("File Deleted");
+})
