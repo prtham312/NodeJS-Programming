b1b93600bc3a02044b6d437b47f74c22425f9b83
prtham312 <pb3488@srmist.edu.in>
2025-04-08 14:41:53 +0530

    Implementation of url and filesystem together.

---
+++ url_fs.js
@@ -0,0 +1,20 @@
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+
+http.createServer(function(req,res){
+    var q = url.parse(req.url , true);
+    var fileName = '.' + q.pathname;
+    fs.readFile(fileName , function(err , data){
+        if (err) {
+            res.writeHead(404, {'Content-Type': 'text/html'});
+            return res.end("404 Not Found");
+          } 
+          res.writeHead(200, {'Content-Type': 'text/html'});
+          res.write(data);
+          return res.end();
+    })
+}).listen(8080)
+
+
+//Here we parsed the url and got it filename and then by using file function we read the file in our local system and showed output on page
