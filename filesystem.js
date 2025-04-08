9b79e0c546f40950ded2603bac864e0b8a7804ac
prtham312 <pb3488@srmist.edu.in>
2025-04-08 12:23:14 +0530

    fs.append() and created a new file Mynewfile1.txt through it

--- filesystem.js
+++ filesystem.js
@@ -1,14 +1,24 @@
-var http = require('http');
+// var http = require('http');
 var fs = require('fs');
 
-http.createServer(function(req,res){
-    fs.readFile('demo1.html' , function(err , data){
-        if(err){
-            res.writeHead(404 , {"content-type" : 'text/plain'})
-            res.write("Error 404 not found")
-        }
-        res.writeHead(200 , {"content-type" : 'text/html'});
-        res.write(data);
-        return res.end();
-    });
-}).listen(8080)
+// http.createServer(function(req,res){
+//     // fs.readFile('demo1.html' , function(err , data){
+//     //     if(err){
+//     //         res.writeHead(404 , {"content-type" : 'text/plain'})
+//     //         res.write("Error 404 not found")
+//     //     }
+//     //     res.writeHead(200 , {"content-type" : 'text/html'});
+//     //     res.write(data);
+//     //     return res.end();
+//     // });
+// }).listen(8080)
+
+fs.appendFile('mynewfile1.txt', 'Hello content!', function (err) {
+    if (err) throw err;
+    console.log('Saved!');
+  });
+
+/* 
+will throw an error if file not found
+fs.append data to a file and if file is not present it will create a new file
+*/
