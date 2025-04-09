c7a8fb701e6e35eaf9173f55ee44440fa9b82216
prtham312 <pb3488@srmist.edu.in>
2025-04-09 10:38:05 +0530

    Class Implementation

---
+++ OOPS.js
@@ -0,0 +1,22 @@
+//class 
+{
+    class Person{
+        constructor(name , age){
+            this.name = name;
+            this.age = age;
+        }
+
+        greet(){
+            return `HI My name is ${this.name}.`
+        }
+
+        isAge(){
+            return this.age>=18;
+        }
+    }
+
+    let p1 = new Person( "Anik" , 19);
+    let p2 = new Person("Diwas" , 16);
+    console.log(p1.greet());
+    console.log(p2.isAge());
+}
