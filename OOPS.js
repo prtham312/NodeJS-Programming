f04835b087c6a0f44aab92e4ada5811422aaa2a6
prtham312 <pb3488@srmist.edu.in>
2025-04-09 11:57:57 +0530

    Prototypes in Object

--- OOPS.js
+++ OOPS.js
@@ -68,3 +68,33 @@
       //this in object refers to windows so it will show output as undefine
       //this in class take context from nearby context so it will define value.
 }
+
+{
+
+
+    //Prototype
+    function Person(name){
+        this.name = name;
+
+        Person.prototype.sayHello = () => {
+            console.log(`Hi my name is ${this.name}`)
+        }
+
+        Person.prototype.toString = function(){
+            return this.name
+        }
+    }
+
+    let p1 = new Person(32);
+    p1.sayHello();
+    //sayHello() is not inside the constructor — it’s in the prototype. All instances share it, saving memory.
+    console.log(p1.toString()); //it will give output [Object ,Object] as it is taking tostring() prototype from object unless we define it in Person also
+
+    /*
+        So when you call p1.sayHello(), JavaScript checks:
+        Is sayHello in p1?
+        If not, look in p1.__proto__ (which is Person.prototype)
+        If still not found, look in Object.prototype
+    */
+
+}
