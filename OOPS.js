2898b5bec0e6d6c6069f46601edeea0cb7955ad8
prtham312 <pb3488@srmist.edu.in>
2025-04-09 11:37:03 +0530

    'this' reference in class and objects

--- OOPS.js
+++ OOPS.js
@@ -25,3 +25,46 @@
     console.log(p2.name);
 
 }
+
+
+{
+    // regular function vs and arrow function for this reference
+    class Test {
+        constructor() {
+          this.name = "NodeJS";
+        }
+      
+        regularFunc() {
+          console.log("Regular:", this.name);
+        }
+      
+        arrowFunc = () => {
+          console.log("Arrow:", this.name);
+        }
+      }
+      
+      let t = new Test();
+      t.regularFunc(); // Regular: NodeJS
+      t.arrowFunc();   // Arrow: NodeJS
+      let j = t;
+      j.arrowFunc();
+}
+{
+    const obj = {
+        value: 42,
+        
+        regularMethod: function () {
+          console.log(this.value); // `this` refers to `obj`
+        },
+        arrowMethod : () => {
+            console.log(this.value); // `this` refers to the global object, not `obj`
+          },
+      };
+      
+      obj.arrowMethod(); // Output: undefined (or error in strict mode)
+      obj.regularMethod(); // Output: 42
+      
+
+      //this in object refers to windows so it will show output as undefine
+      //this in class take context from nearby context so it will define value.
+}
