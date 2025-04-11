42dee5ac0f3b9374b7c4abaa0e4faaa4ec5154b3
prtham312 <pb3488@srmist.edu.in>
2025-04-11 10:52:25 +0530

    Nested if

--- Conditional_Branching.js
+++ Conditional_Branching.js
@@ -1,3 +1,4 @@
+{
 let num = 1;
 if(num%3 === 0 && num%5 === 0){
     console.log(`${num} is divisible by 3 and 5.`)
@@ -9,3 +10,17 @@ else if(num%3 !== 0 && num%5 ===0){
     console.log(`${num} is divisible by 5.`)
 }
     else console.log(`${num} is not divisible either by 3 or 5.`)
+}
+
+{
+    //Nested if
+ let user = {
+    isLoggedIn : true ,
+    isAdmin : true
+}
+if(user.isLoggedIn){
+    if(user.isAdmin) console.log("Welcome Admin");
+    else console.log("Welcome User")
+}
+else console.log("Login Now")
+}
