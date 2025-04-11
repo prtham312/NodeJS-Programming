19d85f032fac6c1c82eac375c18bfe650adae343
prtham312 <pb3488@srmist.edu.in>
2025-04-11 11:58:55 +0530

    Fibonnaci Series

--- Recursion.js
+++ Recursion.js
@@ -6,3 +6,13 @@
     }
     console.log(factorial(5))
 }
+
+
+{
+    //Fibonnaci Series
+    function Fibonnaci(n){
+        if(n <= 1) return 1;
+        return Fibonnaci(n-1) + Fibonnaci(n-2);
+    }
+    console.log(Fibonnaci(6))
+}
