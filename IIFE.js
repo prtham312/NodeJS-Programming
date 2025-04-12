9b0db30bfc39e70c97223d37f21c35c43642de8e
prtham312 <pb3488@srmist.edu.in>
2025-04-12 12:34:37 +0530

    Basic IIFE function

---
+++ IIFE.js
@@ -0,0 +1,8 @@
+//IIFE stands for immediately Invoked Function Expression
+//as this function is invoked by itself it is used for private scope , such as encapsulation
+
+
+
+(function(){
+    console.log("Hello")
+})();
