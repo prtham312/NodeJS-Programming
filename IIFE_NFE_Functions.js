42d3aa1c2bf4fcd9bca7343450f167002b73ef04
prtham312 <pb3488@srmist.edu.in>
2025-04-12 12:41:14 +0530

    NFE Factorial Function

--- IIFE_NFE_Functions.js
+++ IIFE_NFE_Functions.js
@@ -13,3 +13,14 @@
 (function(a,b){
     console.log(a+b);
 })(3,4)
+
+
+
+//An NFE is a function expression that has a name. This name is available only inside the function itself (useful for recursion or debugging).
+
+const Factorial = function fact(n){
+        if(n<=1) return 1;
+        return n * fact(n-1);   
+}
+console.log(Factorial(3)) //Here factorial function is used , fact cant be accessed here.
+// console.log(fact(3)) //Stating error that fact is not defined.
