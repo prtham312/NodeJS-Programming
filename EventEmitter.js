ec0af0fef9ab87dcc4a72ae48c4d2d9268bb1106
prtham312 <pb3488@srmist.edu.in>
2025-04-12 12:09:57 +0530

    Event with Parameters

--- EventEmitter.js
+++ EventEmitter.js
@@ -1,6 +1,17 @@
 const EventEmitter = require('events')
 const event = new EventEmitter();
 event.on('greet', () => {
-    console.log("Hola! this is a event lister code");
+    console.log("Hola! this is a event lister code")
 })
 event.emit('greet')
+
+
+// Event with parameters
+event.on('greet' , (name) => {
+    console.log(`Hola ${name}! , How are you`)
+})
+
+event.emit('greet' , 'Thala')
+event.emit('greet' , 'Reason')
+// While running this code
+//Even after giving parameters , basic function is working
