9abd7046fc34a4f89f1c647d8a3667fd431619ba
prtham312 <pb3488@srmist.edu.in>
2025-04-12 17:06:39 +0530

    Practice Question

---
+++ prac.js
@@ -0,0 +1,27 @@
+let arr = [2,3,4,6,5,7,3,1]
+let count  = 0 ;
+let flag= 0;
+if(arr[0]<=arr[1]) flag=1;
+if(arr[0]>arr[1]) flag=-1;
+for(let i = 0 ; i< arr.length ; i++){
+    
+    if(flag==1){
+        if(arr[i]<arr[i+1]){
+            continue;
+        }
+        else{
+            count++;
+            flag = -1;
+        }
+    }
+    if(flag==-1){
+        if(arr[i]>arr[i+1]){
+            continue;
+        }
+        else{
+            count++;
+            flag = 1;
+        }
+    } 
+}
+console.log(count)
