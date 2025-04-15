e03c03225d058dd5d7c9f72e9c1646cdf72e873f
prtham312 <pb3488@srmist.edu.in>
2025-04-15 15:27:32 +0530

    Call and Apply Example

--- Call_&_Apply.js
+++ Call_&_Apply.js
@@ -13,3 +13,14 @@ user1.Print.apply(user2 , [32])
 // call and apply do the same thing
 // only diffrence is call pass the (argument,parameters) where parameters numbers are known by user.
 // in apply instead of individual arguments it passes full array (argument , array[parameters]).
+
+
+function introduce(greeting, punctuation) {
+    console.log(greeting + ", " + this.name + punctuation);
+  }
+  
+  const user = { name: "Bob" };
+  
+  introduce.call(user, "Hi", "!");
+  introduce.apply(user, ["Hello", "."]);
+  
