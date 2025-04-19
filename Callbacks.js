c02219eea5c3709aee0cc6f343e0c4a577c04afe
prtham312 <pb3488@srmist.edu.in>
2025-04-19 11:14:04 +0530

    Custom Callback

---
+++ Callbacks.js
@@ -0,0 +1,26 @@
+function login(user , password , callback){
+    if(user  && password ){
+        console.log("Validating.....")
+    }
+    else {
+        console.log('Not a valid user or password')
+    }
+    setTimeout( ()=>{
+        if(user=="admin" && password=="admin"){
+            callback(null , "succesfull login")
+        }
+        else 
+        callback("Login failed" , null)
+    }
+    ,1500)
+}
+function handlelogin(error , message){
+    if(!error){
+        console.log(message)
+    }
+    else{
+        console.log(error)
+    }
+}
+
+login("admin" , "admins" , handlelogin)
