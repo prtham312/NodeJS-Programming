41b8cc8a549696be376406257053d6a7823da0a1
prtham312 <pb3488@srmist.edu.in>
2025-04-19 12:30:04 +0530

    filter in IIFE

--- Practice.js
+++ Practice.js
@@ -1,6 +1,13 @@
+  //IIFE
+
 {
-    //IIFE
     (function(a,b){
         console.log(a+b)
     })(3,7)
+}
+
+{
+    (function(array){
+        console.log(array.filter(item => item>=2 && item<11))
+    })([1,34,2,1,3,5,32,11,2,3,44,6,7,88,9,11,2])
 }
