19d047c18f6b9da7d79eb8db4e93ab2abc4b4674
prtham312 <pb3488@srmist.edu.in>
2025-04-19 14:28:12 +0530

    Object with IIFE

--- Practice.js
+++ Practice.js
@@ -11,3 +11,10 @@
         console.log(array.filter(item => item>=2 && item<11))
     })([1,34,2,1,3,5,32,11,2,3,44,6,7,88,9,11,2])
 }
+{
+    (function(object){
+        console.log(object.user + " works as " + object.profession)
+    })({user : "Saleem",
+        profession : "Tailor"
+    })
+}
